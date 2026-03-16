df8c7e92a23513a874953e0aa7828b683cc593e4
Sameed Siddiqui <sameedsiddiqui9@gmail.com>
2026-03-17 01:44:40 +0500

    env changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -131,10 +131,10 @@ export default function Contact() {
 
     try {
       await emailjs.send(
-        import.meta.env.VITE_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+        import.meta.env.EMAILJS_SERVICE_ID,
+        import.meta.env.EMAILJS_TEMPLATE_ID,
         templateParams,
-        import.meta.env.VITE_EMAILJS_PUBLIC_KEY,
+        import.meta.env.EMAILJS_PUBLIC_KEY,
       );
 
       setStatus("success");
